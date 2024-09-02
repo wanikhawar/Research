597c4f76b1cd11526e2c732d12fcf6d9c03ed386
Khawar Zamman Wani <wanikhawar@gmail.com>
2024-09-02 15:03:05 +0530

    add constants

--- Abaqus/get_velocity.py
+++ Abaqus/get_velocity.py
@@ -12,12 +12,13 @@ odb = openOdb(path="odb_filename.odb")
 # Frames
 fs = odb.steps["step_name"].frames
 
-start_frame = 600
+START_FRAME = 400
+END_FRAME = len(fs)
 
 # Open files velocity_x.dat and velocity_y.dat in write mode
 with open("velocity_x.dat", "w+") as vx_f, open("velocity_y.dat", "w+") as vy_f:
-    # Iterate over frames in 'flow' step
-    for i in range(start_frame, len(fs)):
+    # Iterate over frames in 'step_name' step
+    for i in range(START_FRAME, END_FRAME):
         # Extract the velocity data for the current frame
         velocities = fs[i].fieldOutputs["V"].values
 
