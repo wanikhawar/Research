ccb8504fe27b4ae7e4160586743c92149e7bb4ff
Khawar Zamman Wani <wanikhawar@gmail.com>
2024-09-02 17:53:05 +0530

    modify get_velocity.py

--- Abaqus/get_vorticity.py
+++ Abaqus/get_vorticity.py
@@ -1,31 +1,36 @@
-# extracts vorticity data from an ODB file and writes it to three files, vorticity_x.dat, vorticity_y.dat, and vorticity_z.dat
-
-from odbAccess import *
 from abaqusConstants import *
+from odbAccess import *
 from odbMaterial import *
 from odbSection import *
-import numpy as np
 
 # Open ODB
-odb = openOdb(path='odb_filename.odb')
+odb = openOdb(path="odb_filename.odb")
 
 # Frames
-fs = odb.steps['step_name'].frames
+fs = odb.steps["step_name"].frames
 
-start_frame = 600
+START_FRAME = 600
+END_FRAME = len(fs)
 
-# Open files velocity_x.dat and velocity_y.dat in write mode
-with open('vorticity_x.dat', 'w+') as vortx_f, open('vorticity_y.dat', 'w+') as vorty_f, open('vorticity_z.dat', 'w+') as vortz_f:
-    # Iterate over frames in 'flow' step
-    for i in range(start_frame, len(fs)):
-        # Create dictionary of node labels and x, y, and z vorticities
-        vort = {vorticity.nodeLabel: vorticity.data[:3] for vorticity in fs[i].fieldOutputs['VORTICITY'].values}
+with open("vorticity_x.dat", "w") as vortx_f, open(
+    "vorticity_y.dat", "w"
+) as vorty_f, open("vorticity_z.dat", "w") as vortz_f:
+    for i in range(START_FRAME, END_FRAME):
+        # Extract the vorticity data for the current frame
+        vorticities = fs[i].fieldOutputs["VORTICITY"].values
+
+        print("WRITING STEP {0} DATA".format(i))
+
         # Write vorticities to files
-        vortxs = np.array(list(vort.values()))[:, 0]
-        vortys = np.array(list(vort.values()))[:, 1]
-        vortzs = np.array(list(vort.values()))[:, 2]
-        vortx_f.write(','.join(map(str, vortxs)) + '\n')
-        vorty_f.write(','.join(map(str, vortys)) + '\n')
-        vortz_f.write(','.join(map(str, vortzs)) + '\n')
-
-
+        vortx_f.write(
+            ",".join("{:.6f}".format(vorticity.data[0]) for vorticity in vorticities)
+            + "\n"
+        )
+        vorty_f.write(
+            ",".join("{:.6f}".format(vorticity.data[1]) for vorticity in vorticities)
+            + "\n"
+        )
+        vortz_f.write(
+            ",".join("{:.6f}".format(vorticity.data[2]) for vorticity in vorticities)
+            + "\n"
+        )
