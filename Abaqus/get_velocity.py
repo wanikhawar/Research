b34278d2e36ce7751da68cd9ff093f65c61c8a69
Khawar Zamman Wani <wanikhawar@gmail.com>
2024-09-02 15:38:52 +0530

    remove numpy dependency and optimize code

--- Abaqus/get_velocity.py
+++ Abaqus/get_velocity.py
@@ -1,6 +1,5 @@
 # extracts velocity data from an ODB file and writes it to two files, velocity_x.dat and velocity_y.dat
 
-import numpy as np
 from abaqusConstants import *
 from odbAccess import *
 from odbMaterial import *
@@ -16,20 +15,15 @@ START_FRAME = 400
 END_FRAME = len(fs)
 
 # Open files velocity_x.dat and velocity_y.dat in write mode
-with open("velocity_x.dat", "w+") as vx_f, open("velocity_y.dat", "w+") as vy_f:
-    # Iterate over frames in 'step_name' step
+with open("velocity_x.dat", "w") as vx_f, open("velocity_y.dat", "w") as vy_f:
     for i in range(START_FRAME, END_FRAME):
         # Extract the velocity data for the current frame
         velocities = fs[i].fieldOutputs["V"].values
 
-        # Pre-allocate lists for x and y velocities
-        velxs = np.empty(len(velocities))
-        velys = np.empty(len(velocities))
-
-        # Populate the lists directly
-        for j, velocity in enumerate(velocities):
-            velxs[j], velys[j] = velocity.data[:2]
-
-        # Write velocities to files
-        np.savetxt(vx_f, velxs[None], delimiter=",", fmt="%f")
-        np.savetxt(vy_f, velys[None], delimiter=",", fmt="%f")
+        # Use a generator expression to avoid creating intermediate arrays
+        vx_f.write(
+            ",".join(f"{velocity.data[0]:.6f}" for velocity in velocities) + "\n"
+        )
+        vy_f.write(
+            ",".join(f"{velocity.data[1]:.6f}" for velocity in velocities) + "\n"
+        )
